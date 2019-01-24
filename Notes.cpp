c1dee17dffeb6039e1dd65f78dc7fb7d9b30a51c
oklodhi <okhanlodhi@gmail.com>
2019-01-23 20:10:13 -0600

    Jan 23

---
+++ Notes.cpp
@@ -0,0 +1,48 @@
+// C++ is an object oriented programming language
+    // Encapsulation, Polymorphism, Inheritance
+
+// Every program starts with
+    // #include <iostream>
+    // using namespace std;
+
+// New C++11 types
+    auto x = expression;
+    decltype(x*3.5) y;
+
+// Data types
+    int _int = 10;
+    long _long = 9999999999;
+    float _float = 10.983;
+    double _double = 123.45678;
+    char _char = a;
+    bool _bool = true;
+
+// Escape Characters
+    // \n - new line
+    // \t - tab space
+    // \a - alert
+    // \\ - 1 backslash
+    // \' - single quote
+    // \" - double quote
+
+// Raw String literals
+    string s = R"(\t\\t\n)";
+    // print: \t\\t\n
+
+// Constants
+    // value cannot be changed
+
+// Type casting
+    // Converting one type to another type
+
+// Short Hand operator
+    // ++. --
+
+// Function Overloading (pre and post increment)
+    var++; // uses value then adds 1
+    ++var;  // adds value then add 1
+
+// Input and output
+    cin, cout, cerr;
+    <iostream>
+
