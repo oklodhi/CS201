0699ffc12dd39f4d4fe95160c7de89be47618389
Omer Khan <okhanlodhi@gmail.com>
2019-01-28 20:07:34 -0600

    Jan 28

--- Notes.cpp
+++ Notes.cpp
@@ -38,11 +38,54 @@
 // Short Hand operator
     // ++. --
 
-// Function Overloading (pre and post increment)
+// Function Overloading (pre and post increment / decrement)
     var++; // uses value then adds 1
     ++var;  // adds value then add 1
 
 // Input and output
-    cin, cout, cerr;
-    <iostream>
+    // cin, cout, cerr;
+    // <iostream>
 
+// Logical Operators
+	// and &&
+	// or ||
+	// not equal != 
+	// is equal == different than single = assignment
+
+
+	// greater than, equal to etc. > < >= <=
+	// logical operators have lowest priority
+
+// Strong enums and enum classes
+	// creating new class means creating new type
+	// aka enums create a new type
+	enum class Days = { Sun, Mon, Tues, Wed, Thur, Fri, Sat };
+	Days d = Days::Tue; 
+
+// if-else statements
+	hrs = 40
+	if (hrs > 40) { // block or compound statement
+		return true; // single statement 
+	}
+	else {
+		return false; // semi-colon ; is called a statement
+	}
+
+// Nested loops / statements 
+	// Having multiple statements and loops inside one another in a "nest"
+
+// Switch statements 
+	int 1_or_2; 
+	cin >> 1_or_2; 
+
+	switch (1_or_2)
+	{
+	case 1:
+		cout << "You picked 1";
+		break;
+	case 2:
+		cout << "You picked 2";
+		break;
+	default:
+		cout << "Not a valid number";
+	}
