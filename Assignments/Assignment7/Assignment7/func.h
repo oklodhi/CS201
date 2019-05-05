607835ce6abb936b936c8c50add1aab27cccc16d
Omer Khan <okhanlodhi@gmail.com>
2019-05-05 00:39:33 -0500

    Initial commit

---
+++ Assignments/Assignment7/Assignment7/func.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <map>
+
+// opens and reads file that is passed in
+void open_and_read(std::string filename);
---
+++ Assignments/Assignment7/Assignment7/main.cpp
@@ -0,0 +1,10 @@
+// Omer Khan 
+// Assignment 7
+// CS201R - GHARIBI
+// MAY 5, 2019
+
+#include "func.h"
+
+int main() {
+
+}
